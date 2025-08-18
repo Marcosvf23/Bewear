ac62b171721b2756b888f87efa471f1d8fba0d3f
Marcos Fernandes <marcosvf23@hotmail.com>
2025-08-18 12:11:35 -0300

    feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
+import { desc } from "drizzle-orm";
 import Image from "next/image";
 
 import CategorySelector from "@/components/common/category-selector";
+import Footer from "@/components/common/footer";
 import { Header } from "@/components/common/header";
 import ProductList from "@/components/common/product-list";
 import { db } from "@/db";
+import { productTable } from "@/db/schema";
 
 const Home = async () => {
   const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
+  const newlyCreatedProducts = await db.query.productTable.findMany({
+    orderBy: [desc(productTable.createdAt)],
     with: {
       variants: true,
     },
@@ -19,7 +28,7 @@ const Home = async () => {
       <div className="space-y-6">
         <div className="px-5">
           <Image
-            src="/banner-01.png"
+            src="/banner01.png"
             alt="Leve uma vida com estilo"
             height={0}
             width={0}
@@ -36,7 +45,7 @@ const Home = async () => {
 
         <div className="px-5">
           <Image
-            src="/banner-02.png"
+            src="/banner02.png"
             alt="Leve uma vida com estilo"
             height={0}
             width={0}
@@ -44,6 +53,9 @@ const Home = async () => {
             className="h-auto w-full"
           />
         </div>
+
+        <ProductList products={newlyCreatedProducts} title="Novos produtos" />
+        <Footer />
       </div>
     </>
   );
