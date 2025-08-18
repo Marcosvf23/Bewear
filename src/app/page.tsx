29f7ac2716e118d68d2bd5899604073d16292388
Marcos Fernandes <marcosvf23@hotmail.com>
2025-08-18 11:08:28 -0300

    feat: add product list and product item components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,45 @@
-import { Header } from "@/components/common/header";
+import { desc } from "drizzle-orm";
+import Image from "next/image";
 
-const Home = () => {
+import { Header } from "@/components/common/header";
+import ProductList from "@/components/common/product-list";
+import { db } from "@/db";
+import { productTable } from "@/db/schema";
+
+const Home = async () => {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
   return (
     <>
       <Header />
+      <div className="space-y-6">
+        <div className="px-5">
+          <Image
+            src="/banner01.png"
+            alt="Leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vh"
+            className="h-auto w-full"
+          />
+        </div>
+
+        <ProductList products={products} title="Mais vendidos" />
+
+        <div className="px-5">
+          <Image
+            src="/banner02.png"
+            alt="Leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vh"
+            className="h-auto w-full"
+          />
+        </div>
+      </div>
     </>
   );
 };
