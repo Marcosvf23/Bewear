9d8f664c0cec58974fd57cfa92aaa80e850e1e5e
Marcos Fernandes <marcosvf23@hotmail.com>
2025-08-18 12:03:49 -0300

    feat: add category selector

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import { desc } from "drizzle-orm";
 import Image from "next/image";
 
+import CategorySelector from "@/components/common/category-selector";
 import { Header } from "@/components/common/header";
 import ProductList from "@/components/common/product-list";
 import { db } from "@/db";
-import { productTable } from "@/db/schema";
 
 const Home = async () => {
   const products = await db.query.productTable.findMany({
@@ -12,17 +11,19 @@ const Home = async () => {
       variants: true,
     },
   });
+  const categories = await db.query.categoryTable.findMany({});
+
   return (
     <>
       <Header />
       <div className="space-y-6">
         <div className="px-5">
           <Image
-            src="/banner01.png"
+            src="/banner-01.png"
             alt="Leve uma vida com estilo"
             height={0}
             width={0}
-            sizes="100vh"
+            sizes="100vw"
             className="h-auto w-full"
           />
         </div>
@@ -30,12 +31,16 @@ const Home = async () => {
         <ProductList products={products} title="Mais vendidos" />
 
         <div className="px-5">
+          <CategorySelector categories={categories} />
+        </div>
+
+        <div className="px-5">
           <Image
-            src="/banner02.png"
+            src="/banner-02.png"
             alt="Leve uma vida com estilo"
             height={0}
             width={0}
-            sizes="100vh"
+            sizes="100vw"
             className="h-auto w-full"
           />
         </div>
